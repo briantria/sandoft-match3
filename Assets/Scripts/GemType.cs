bc9f07c485fd94e8b1b305959d94a8ac768c4de5
brian <brian.tria.se@gmail.com>
2023-10-28 20:31:02 -0700

    create gem and gemtype

---
+++ Assets/Scripts/GemType.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GemType 
+{
+    Apple,
+    Pineapple,
+    Banana,
+    Orange,
+    Strawberry,
+    Kiwi
+}
