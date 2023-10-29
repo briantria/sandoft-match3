29e6506fb32490db0d56eb2137d686cc24304eee
brian <brian.tria.se@gmail.com>
2023-10-28 20:55:43 -0700

    visualize gem type

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -6,12 +6,14 @@ using UnityEngine.UI;
 public class Gem : MonoBehaviour
 {
     [SerializeField] private GemType _gemType;
-    [SerializeField] private Image _icon;
+    [SerializeField] private Image _displayIcon;
+    [SerializeField] private Sprite[] _icons;
 
     public Vector2 Size = new Vector2(100, 100);
 
     public void Setup(GemType type)
     {
         _gemType = type;
+        _displayIcon.sprite = _icons[(int)type];
     }
 }
